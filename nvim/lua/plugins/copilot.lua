3ebf1c4a6342057a140b4391118d6f09cab585aa
claytonsilva <clayton.silva@grupoboticario.com.br>
2025-05-24 16:32:16 -0300

    fix: added support copilot for more extentions

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      panel = { enabled = false },
+      filetypes = {
+        yaml = true,
+        markdown = true,
+        help = true,
+        gitcommit = true,
+        gitrebase = true,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+    },
+  },
+}
